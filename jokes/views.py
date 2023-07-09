ff1874fbf2463b17fc6c6984eb7ed1fc73e9bdf7
monrow69 <silvermoon12286@gmail.com>
2023-07-09 14:22:26 -0500

    Project changes

--- jokes/views.py
+++ jokes/views.py
@@ -1,6 +1,20 @@
-from django.views.generic import ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
 from .models import Joke
 
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
+
+
+class JokeDetailView(DetailView):
+    model = Joke
+
+
 class JokeListView(ListView):
     model = Joke
+
+
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
