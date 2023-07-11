40b69bfcfa6c9bf1a2811cbef97e518896922a77
monrow69 <silvermoon12286@gmail.com>
2023-07-10 19:27:54 -0500

    Project changes

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+User = get_user_model()
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Optional Fields', {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name'),
+        }),
+    )
