3c380cdf3d97fb8f0b000f90cbd06d83fe2c98aa
monrow69 <silvermoon12286@gmail.com>
2023-07-16 15:36:03 -0500

    Project changes

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
-from .models import Category, Joke
+from .models import Category, Joke, Tag
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+    list_display = ['tag', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return ('slug', 'created', 'updated')
+        return ()
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
