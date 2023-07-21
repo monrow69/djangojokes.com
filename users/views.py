ccba973137bea895fc93ce572c16f6213135a1fb
monrow69 <silvermoon12286@gmail.com>
2023-07-20 21:13:37 -0500

    Project changes

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.contrib.messages.views import SuccessMessageMixin
 from allauth.account.views import PasswordChangeView
 
 from .forms import CustomUserChangeForm
@@ -10,9 +10,10 @@ from .forms import CustomUserChangeForm
 class CustomPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
     success_url = reverse_lazy('my-account')
 
-class MyAccountPageView(LoginRequiredMixin, UpdateView):
+class MyAccountPageView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     model = get_user_model()
     form_class = CustomUserChangeForm
+    success_message = 'Update Succesful'
     template_name = 'account/my_account.html'
 
     def get_object(self):
