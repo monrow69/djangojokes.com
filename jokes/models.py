5c09fe1c00c4fabb32f9f6e32d056bc4ea7dbee8
monrow69 <silvermoon12286@gmail.com>
2023-07-23 09:17:45 -0500

    Project changes

--- jokes/models.py
+++ jokes/models.py
@@ -4,38 +4,16 @@ from django.urls import reverse
 
 from common.utils.text import unique_slug
 
-class Category(models.Model):
-    class Meta:
-        verbose_name_plural = 'Categories'
-    category = models.CharField(max_length=50)
-    slug = models.SlugField(
-        max_length=50, unique=True, null=False, editable=False
-    )
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    def get_absolute_url(self):
-        return reverse('jokes:category', args=[self.slug])
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            value = str(self)
-            self.slug = unique_slug(value, type(self))
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.category
-
 class Joke(models.Model):
     question = models.TextField(max_length=200)
     answer = models.TextField(max_length=100, blank=True)
     user = models.ForeignKey(
-    settings.AUTH_USER_MODEL, on_delete=models.PROTECT,
-    related_name='jokes'
-)
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT,
+        related_name='jokes'
+    )
     category = models.ForeignKey(
-    'Category', on_delete=models.PROTECT, related_name='jokes'
-)
+        'Category', on_delete=models.PROTECT, related_name='jokes'
+    )
     tags = models.ManyToManyField('Tag', blank=True, related_name='jokes')
     slug = models.SlugField(
         max_length=50, unique=True, null=False, editable=False
@@ -68,9 +46,33 @@ class Joke(models.Model):
     def __str__(self):
         return self.question
 
-class Tag(models.Model):
+
+class Category(models.Model):
+    category = models.CharField(max_length=50)
+    slug = models.SlugField(
+        max_length=50, unique=True, null=False, editable=False
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('jokes:category', args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.category
+
     class Meta:
-        ordering = ['tag']
+        verbose_name_plural = 'Categories'
+        ordering = ['category']
+
+
+class Tag(models.Model):
     tag = models.CharField(max_length=50)
     slug = models.SlugField(
         max_length=50, unique=True, null=False, editable=False
@@ -89,6 +91,8 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.tag
+
+
 class JokeVote(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
