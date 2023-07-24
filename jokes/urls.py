9050fdfbfe567b05f19f674f02b87d94abb055f9
monrow69 <silvermoon12286@gmail.com>
2023-07-24 17:55:53 -0500

    Project changes

--- jokes/urls.py
+++ jokes/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     path('joke/<slug>/vote/', vote, name='ajax-vote'),
     path('', JokeListView.as_view(), name='list'),
     path('category/<slug>/', JokeListView.as_view(), name='category'),
-path('tag/<slug>/', JokeListView.as_view(), name='tag'),
-path('creator/<username>/', JokeListView.as_view(), name='creator'),
+    path('tag/<slug>/', JokeListView.as_view(), name='tag'),
+    path('creator/<username>/', JokeListView.as_view(), name='creator'),
 ]
