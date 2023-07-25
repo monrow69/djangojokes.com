98b3442910057e7072337baf194d2ad7f2de2da7
monrow69 <silvermoon12286@gmail.com>
2023-07-25 12:39:16 -0500

    Project changes

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
+from common.admin import DjangoJokesAdmin
+
 from .models import Category, Joke, JokeVote, Tag
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(DjangoJokesAdmin):
     model = Category
     list_display = ['category', 'created', 'updated']
 
@@ -14,18 +16,32 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 @admin.register(Joke)
-class JokeAdmin(admin.ModelAdmin):
+class JokeAdmin(DjangoJokesAdmin):
     model = Joke
+
+    # List attributes
+    date_hierarchy = 'updated'
     list_display = ['question', 'created', 'updated']
+    list_filter = ['updated', 'category', 'tags']
+    ordering = ['-updated']
+    search_fields = ['question', 'answer']
+
+    # Form Attributes
+    autocomplete_fields = ['tags', 'user']
+    radio_fields = { 'category': admin.HORIZONTAL}
+
+    def vote_summary(self, obj):
+        return f'{obj.num_votes} votes. Rating: {obj.rating}.'
 
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
-            return ('slug', 'created', 'updated')
+            return ('slug', 'created', 'updated', 'vote_summary')
+
         return ()
 
 
 @admin.register(JokeVote)
-class JokeVoteAdmin(admin.ModelAdmin):
+class JokeVoteAdmin(DjangoJokesAdmin):
     model = JokeVote
     list_display = ['joke', 'user', 'vote']
 
@@ -35,10 +51,12 @@ class JokeVoteAdmin(admin.ModelAdmin):
         return ()
 
 
+
 @admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
+class TagAdmin(DjangoJokesAdmin):
     model = Tag
     list_display = ['tag', 'created', 'updated']
+    search_fields = ['tag']
 
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
